233d5c291313c4e6cc5c83c39b526b5ef832b3eb
Shriya Tatachar <62716775+TatShri@users.noreply.github.com>
2024-07-07 16:37:39 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -2,44 +2,46 @@
 #include <iostream>
 using namespace std;
 
-bool TemperaureIsOk(float temperature)
-{
-     if (temperature < 0 || temperature > 45) 
-     {
-        cout << "Temperature out of range!\n";
-        return false;
-    }
-    return true;
+void printMessage(const string& message) {
+    cout << message << endl;
 }
-bool socIsOk(float soc)
-{
-    if (soc < 20 || soc > 80) 
-    {
-        cout << "State of Charge out of range!\n";
+
+bool checkInRange(float value, float min, float max, const string& rangeType) {
+    if (value < min || value > max) {
+        printMessage(rangeType + " out of range!");
         return false;
+    } else {
+        printMessage(rangeType + " in range.");
     }
     return true;
 }
 
-bool chargeRateIsOk(float chargeRate)
-{
-    if (chargeRate > 0.8) 
-    {
-        cout << "Charge Rate out of range!\n";
-        return false;
-    }
-    return true;
+bool TemperatureIsOk(float temperature) {
+    return checkInRange(temperature, 0, 45, "Temperature");
 }
-bool batteryIsOk(float temperature, float soc, float chargeRate) 
-{
-    bool tempIsOk = TemperaureIsOk(temperature);
+
+bool socIsOk(float soc) {
+    return checkInRange(soc, 20, 80, "State of Charge");
+}
+
+bool chargeRateIsOk(float chargeRate) {
+    return checkInRange(chargeRate, 0, 0.8, "Charge Rate");
+}
+
+bool batteryIsOk(float temperature, float soc, float chargeRate) {
+    bool tempIsOk = TemperatureIsOk(temperature);
     bool stcIsOk = socIsOk(soc);
     bool crIsOk = chargeRateIsOk(chargeRate);
-    return (tempIsOk&stcIsOk&crIsOk);
+    return (tempIsOk && stcIsOk && crIsOk);
 }
 
-int main() 
-{
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+int main() {
+    assert(batteryIsOk(25, 70, 0.7) == true);  // All values within range
+    assert(batteryIsOk(50, 85, 0) == false);   // Temperature and SOC out of range
+    assert(batteryIsOk(0, 20, 0.8) == true);   // All values at the boundary of their range
+    assert(batteryIsOk(-1, 70, 0.5) == false); // Temperature out of range
+    assert(batteryIsOk(25, 10, 0.5) == false); // SOC out of range
+    assert(batteryIsOk(25, 70, 0.9) == false); // Charge Rate out of range
+    assert(batteryIsOk(45, 80, 0.8) == true);  // All values at the upper boundary of their range
+    assert(batteryIsOk(0, 20, 0) == true);     // All values at the lower boundary of their range
 }
