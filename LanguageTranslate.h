936712b22e05e0d2fc2014f53fbac6b7dec8b546
Shriya Tatachar <62716775+TatShri@users.noreply.github.com>
2024-07-18 13:25:07 +0530

    Create LanguageTranslate.h

---
+++ LanguageTranslate.h
@@ -0,0 +1,17 @@
+#ifndef LANGUAGETRANSLATE_H
+#define LANGUAGETRANSLATE_H
+
+#include <string>
+#include <map>
+
+// Declare the languagetranslation_map and current language
+extern std::map<std::string, std::map<std::string, std::string>> languagetranslation_map;
+extern std::string outputlanguage;
+
+// Function to set the output language
+void setoutputlanguage(const std::string& language);
+
+// Function to translate messages
+std::string languagetranslate(const std::string& key);
+
+#endif // LANGUAGETRANSLATE_H
